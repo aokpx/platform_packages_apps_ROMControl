cb4b5e8b9ee34e3040d5b087883e10c65d1299bb
Luis Cruz <ljc2491@gmail.com>
2013-03-21 03:12:09 -0500

    ROMControl: Port hardware key rebinding to aokpx (2/2) exclude overflow from: https://github.com/SlimRoms/packages_apps_Settings/commit/32e385c08c3b48dbaaabc2664eeddf6453c31b45 Signed-off-by: Luis Cruz <ljc2491@gmail.com>
    Change-Id: I1fe05d4e9d1542f0f24cf09f0ab34d2cc83192dd
    
    fixes
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>
    
    Change-Id: Id0d5f43fb88a3fdacad959ec5c2ef1a953412b5d

--- src/com/aokp/romcontrol/fragments/Advanced.java
+++ src/com/aokp/romcontrol/fragments/Advanced.java
@@ -3,14 +3,20 @@ package com.aokp.romcontrol.fragments;
 
 import java.io.File;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.IWindowManager;
 
 import com.aokp.romcontrol.AOKPPreferenceFragment;
 import com.aokp.romcontrol.R;
@@ -20,19 +26,35 @@ import com.aokp.romcontrol.util.Helpers;
 public class Advanced extends AOKPPreferenceFragment {
 
     private static final String TAG = "Advanced";
+    private static final String HARDWARE_SETTINGS = "hardware_category";
+    private static final String KEY_HARDWARE_KEYS = "hardware_keys";
 
     CheckBoxPreference mXlog;
+    private PreferenceCategory mHardware;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs_advanced);
+        PreferenceScreen prefs = getPreferenceScreen();
+        mHardware = (PreferenceCategory) prefs.findPreference(HARDWARE_SETTINGS);
 
         mXlog = (CheckBoxPreference)findPreference("xlog");
         mXlog.setChecked(!new File("/system/bin/logcat").exists());
         if (mXlog.isChecked()) {
             mXlog.setSummary("Logging disabled, please enable before requesting support via XDA");
+        }
+
+        // Only show the hardware keys config on a device that does not have a navbar
+        IWindowManager windowManager = IWindowManager.Stub.asInterface(
+                ServiceManager.getService(Context.WINDOW_SERVICE));
+        try {
+            if (windowManager.hasNavigationBar()) {
+                mHardware.removePreference(findPreference(KEY_HARDWARE_KEYS));
+            }
+        } catch (RemoteException e) {
+            // Do nothing
         }
     }
 
